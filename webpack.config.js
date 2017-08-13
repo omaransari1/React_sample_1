ec4e2be8769d309b6baa9bc6778cec5f788e5b8b
Omar Ansari <omaransari1@gmail.com>
2017-08-13 18:10:35 -0500

    Add babel loader to webpack config, add devserver

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './app.js',
+
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  }
+};
